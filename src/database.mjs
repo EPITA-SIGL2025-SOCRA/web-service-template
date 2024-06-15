0aa5b790775042831dd7d43448934c5a2aeb6b94
florent fauchille <florentfauchille@florents-MacBook-Pro.local>
2024-06-15 15:39:36 +0200

    v1.1: sort by distance

--- src/database.mjs
+++ src/database.mjs
@@ -2,7 +2,7 @@ import data from "./data.json" assert { type: "json" };
 import { distance } from "./distance.mjs";
 
 /**
- * Returns the tractors around a reference point.
+ * Returns the tractors around a reference point, sorted by shortest distance.
  *
  * @param {*} reference { lat: number, lng: number } object which represents the position of the reference point.
  * @param {*} radiusInKm number which represents the radius in kilometers.
@@ -11,9 +11,12 @@ import { distance } from "./distance.mjs";
  */
 export function getTractorsAround(reference, radiusInKm, limit = 25) {
   const { lat, lng } = reference;
-  const tractors = data.filter((tractor) => {
-    const d = distance({ lat, lng }, tractor.lat_long);
-    return d <= radiusInKm;
-  });
+  const tractors = data
+    .map((tractor) => {
+      const d = distance({ lat, lng }, tractor.lat_long);
+      return { ...tractor, distance: d };
+    })
+    .filter(({ distance }) => distance <= radiusInKm)
+    .sort((a, b) => a.distance - b.distance);
   return tractors.slice(0, limit);
 }
